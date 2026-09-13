b95b1d43497e0bfce16e6dd729e1c9f5b6b0faad
Vishal Verma <161016863+Vishal-5@users.noreply.github.com>
2026-09-13 13:09:02 +0530

    Update main.py

--- impacthub_backend/app/main.py
+++ impacthub_backend/app/main.py
@@ -46,10 +46,7 @@ origins = [
     str(settings.FRONTEND_URL), 
     "https://we-do-sandy.vercel.app",
     "https://impacthub-dnjr.vercel.app",
-    "https://impacthub-dnjr-git-main-tanishs-projects-de6a879e.vercel.app",
-    "https://impacthub-l5umgo7qy-vishal-2821s-projects.vercel.app",
-    "https://impacthub-mu.vercel.app",
-    "https://impacthub-dnjr-a8uiow79p-tanishs-projects-de6a879e.vercel.app",
+    
     "http://localhost:3000",
     "http://localhost:3001,
     ,
